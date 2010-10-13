d75dc4651dc3f0af2e31c1e52063e9ab41f1e4cb
webjunk <webjunk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-13 03:49:26 +0000

    tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/eliminate-notify-email/tags/1.0.0@299502 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ eliminate-notify-email.php
@@ -0,0 +1,58 @@
+<?php
+/* 
+/**
+ * @package e-email
+ * @version 1.00
+ */
+/*
+Plugin Name: Eliminate Notify Email
+Plugin URI: http://webjunkdemo.com/downloads/e-email.zip
+Description: Eliminates New User Notification email to Users and Administrator
+Version: 1.00
+Author: Paul Newman
+Author URI: http://webjunk.com/
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
+
+if ( !function_exists('wp_new_user_notification') ) :
+/**
+ * Notify the blog admin of a new user, normally via email.
+ *
+ * @since 2.0
+ *
+ * @param int $user_id User ID
+ * @param string $plaintext_pass Optional. The user's plaintext password
+ */
+function wp_new_user_notification($user_id, $plaintext_pass = '') {
+	$user = new WP_User($user_id);
+
+	$user_login = stripslashes($user->user_login);
+	$user_email = stripslashes($user->user_email);
+
+	// The blogname option is escaped with esc_html on the way into the database in sanitize_option
+	// we want to reverse this for the plain text arena of emails.
+	$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+
+	$message  = sprintf(__('New user registration on your site %s:'), $blogname) . "\r\n\r\n";
+	$message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
+	$message .= sprintf(__('E-mail: %s'), $user_email) . "\r\n";
+
+	@wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), $blogname), $message);
+
+	if ( empty($plaintext_pass) )
+		return;
+
+	$message  = sprintf(__('Username: %s'), $user_login) . "\r\n";
+	$message .= sprintf(__('Password: %s'), $plaintext_pass) . "\r\n";
+	$message .= wp_login_url() . "\r\n";
+
+	// wp_mail($user_email, sprintf(__('[%s] Your username and password'), $blogname), $message)
+
+}
+endif;
+
+?>
